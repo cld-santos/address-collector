137db4c050735fbbb82c4d54e614c947490b6e74
Claudio Santos <c.santos@consultoriasigma.com.br>
2017-09-25 22:39:32 -0300

    must block a creation of a duplicated address

--- src/address_collector/collector/tests/test_service.py
+++ src/address_collector/collector/tests/test_service.py
@@ -1,6 +1,7 @@
 from django.test import TransactionTestCase
 from ..models import Address
 
+
 class CollectorServiceTestCase(TransactionTestCase):
 
     def test_must_create_an_address(self):
@@ -12,4 +13,25 @@ class CollectorServiceTestCase(TransactionTestCase):
 
         address = Address.objects.create(**data)
 
-        self.assertEqual(data['name'], address.name)    
+        self.assertEqual(data['name'], address.name)
+
+    def test_must_block_create_duplicated_address(self):
+        data_a = {
+            'lat': -48,
+            'lon': -20,
+            'name': 'avenida paulista, 1500, sao paulo'
+        }
+
+        data_b = {
+            'lat': -48,
+            'lon': -20,
+            'name': 'Avenida paulista 1500 Sao Paulo'
+        }
+
+        Address.objects.create(**data_a)
+        try:
+            Address.objects.create(**data_b)
+        except Exception:
+            throw_an_exception = True
+
+        self.assertTrue(throw_an_exception)
